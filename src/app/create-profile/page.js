052408101e2283fe1dfeb7e1f2a1326b26a74186
Mary Sharp <marysharp0721@gmail.com>
2024-10-26 17:09:19 -0500

    add the .env.local file

--- src/app/create-profile/page.js
+++ src/app/create-profile/page.js
@@ -5,7 +5,7 @@
 import { useAuth } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import ProfileForm from "./ProfileForm";
+import { handleSubmit } from "@/app/create-profile/action"; 
 
 export default function CreateProfilePage() {
   const { isSignedIn } = useAuth();
@@ -22,7 +22,41 @@ export default function CreateProfilePage() {
   return (
     <>
       <h1>Create Your Profile</h1>
-      <ProfileForm />
+      <form action={handleSubmit} className="flex flex-col items-center">
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        placeholder="Enter your username"
+        required
+        className="text-orange-600"
+      />
+
+      <label htmlFor="profile_image_url">Profile Image URL:</label>
+      <input
+        type="text"
+        name="profile_image_url"
+        id="profile_image_url"
+        placeholder="Paste your profile image URL"
+        className="text-orange-600"
+      />
+
+      <label htmlFor="bio">Biography:</label>
+      <textarea
+        name="bio"
+        id="bio"
+        placeholder="Tell us about yourself"
+        className="text-orange-600"
+      ></textarea>
+
+      <button
+        type="submit"
+        className="border-rose-400 border-4 bg-yellow-400 text-rose-400 p-2 m-4"
+      >
+        Submit Profile
+      </button>
+    </form>
     </>
   );
 }
