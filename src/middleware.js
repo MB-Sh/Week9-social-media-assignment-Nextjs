2a3041049c440c512dd629bebc5223fae543ca77
Mary Sharp <marysharp0721@gmail.com>
2024-10-26 16:12:53 -0500

    reverting back to minimal change

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,24 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// Define protected routes (e.g., /post and /create-profile)
-const isProtectedRoute = createRouteMatcher(["/post(.*)", "/create-profile(.*)"]);
 
-// Protect routes based on match
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect(); // Requires authentication for protected routes
-  }
+
+//set up a public route homepage and a protected post page
+
+const isProtectedRoute =createRouteMatcher (["/post(.*)"])
+
+// if the protected route match is in the request, protect with authentication
+export default clerkMiddleware(async(auth,req)=>{
+    if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files
+    //the matcher will find matches for public and private(it will trigger authentication) route
+  
+    // the matcher uses regex to find matches.
+    matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
